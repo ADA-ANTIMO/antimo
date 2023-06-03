b4a3b7a952bf68884501e4ce2a72bfe008e23e0f
Dewa Bisma <idewagedebisma@gmail.com>
2023-06-03 20:27:35 +0700

    ANT-128: Finish, ANT-123: Finish view, create utility for safe int division

--- Antimo/Utilities/Utilites.swift
+++ Antimo/Utilities/Utilites.swift
@@ -8,13 +8,27 @@
 import Foundation
 
 struct Utilities {
-    
+    enum Round {
+        case up, down
+    }
     // MARK: - Date Utilities
-    
     static func formattedDate(from date: Date, format: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
         return dateFormatter.string(from: date)
     }
+    
+    static func safeDivideIntRounded(num1:Int, num2:Int, rounded:Round = .up) -> Int {
+        let num1f = Float(num1)
+        let num2f = Float(num2)
+        
+        switch rounded {
+            case .up:
+                return Int(ceilf(num1f/num2f))
+            case .down:
+                return Int(floorf(num1f/num2f))
+            }
+    }
+    
     
 }
