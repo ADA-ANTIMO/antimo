d116736d143e9965ad9c2d3c8e87feb18ff4d8bb
Roli Bernanda <acappemborong@gmail.com>
2023-06-05 00:20:42 +0700

    ANT-133: Add static profile screen

--- Antimo/ViewModels/ProfileViewModel.swift
+++ Antimo/ViewModels/ProfileViewModel.swift
@@ -6,15 +6,84 @@
 //
 
 import SwiftUI
+import PhotosUI
+import CoreTransferable
 
-struct ProfileViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@MainActor
+class ProfileModel: ObservableObject {
+    // MARK: - Profile Details
+    
+    @Published var dogName: String = ""
+    @Published var gender: String = ""
+    @Published var breed: String = ""
+    @Published var age: String = ""
+    @Published var weight: String = ""
+    @Published var bod: String = ""
+    
+    // MARK: - Profile Image
+    
+    @Published private(set) var imageState: ImageState = .empty
+    
+    @Published var imageSelection: PhotosPickerItem? = nil {
+        didSet {
+            if let imageSelection {
+                let progress = loadTransferable(from: imageSelection)
+                imageState = .loading(progress)
+            } else {
+                imageState = .empty
+            }
+        }
+    }
+    
+    enum ImageState {
+        case empty
+        case loading(Progress)
+        case success(Image)
+        case failure(Error)
+    }
+    
+    enum TransferError: Error {
+        case importFailed
+    }
+    
+    struct ProfileImage: Transferable {
+        let image: Image
+        
+        static var transferRepresentation: some TransferRepresentation {
+            DataRepresentation(importedContentType: .image) { data in
+            #if canImport(UIKit)
+                guard let uiImage = UIImage(data: data) else {
+                    throw TransferError.importFailed
+                }
+                let image = Image(uiImage: uiImage)
+                return ProfileImage(image: image)
+            #else
+                throw TransferError.importFailed
+            #endif
+            }
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    private func loadTransferable(from imageSelection: PhotosPickerItem) -> Progress {
+        return imageSelection.loadTransferable(type: ProfileImage.self) { result in
+            DispatchQueue.main.async {
+                guard imageSelection == self.imageSelection else {
+                    print("Failed to get the selected item.")
+                    return
+                }
+                switch result {
+                case .success(let profileImage?):
+                    self.imageState = .success(profileImage.image)
+                case .success(nil):
+                    self.imageState = .empty
+                case .failure(let error):
+                    self.imageState = .failure(error)
+                }
+            }
+        }
     }
 }
 
-struct ProfileViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileViewModel()
-    }
-}
+
