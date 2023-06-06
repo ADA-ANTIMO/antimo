7658bd1b185cc753fddeac37dc8101136f59bee4
Roli Bernanda <acappemborong@gmail.com>
2023-06-06 11:28:35 +0700

    ANT-156: create static reminder screen

--- Antimo/Views/ReminderView/ReminderView.swift
+++ Antimo/Views/ReminderView/ReminderView.swift
@@ -8,38 +8,76 @@
 import SwiftUI
 
 struct ReminderView: View {
+    @State var reminderType: String = ""
+    @State var title: String = ""
+    @State var desc: String = ""
+    @State var time: String = ""
+    @State var frequency: String = ""
+    
+    @State var isSheetPresented: Bool = false
+    
+    private func openReminderForm() {
+        isSheetPresented = true
+    }
+    
+    private func closeReminderForm() {
+        isSheetPresented = false
+    }
+    
     var body: some View {
         ANBaseContainer(toolbar: {
             ANToolbar(title: "Reminder") {
-                NavigationLink(value: Route.addActivities) {
-                    Text("Add Reminder")
-                        .font(.toolbar)
-                        .foregroundColor(Color.anNavigation)
-                }
-                .navigationDestination(for: Route.self) {
-                    $0
-                }
+                Text("Add Reminder")
+                    .font(.toolbar)
+                    .foregroundColor(Color.anNavigation)
+                    .onTapGesture {
+                        openReminderForm()
+                    }
             }
         }, children: {
             ScrollView {
-                Text("CHILDREN")
-                    .frame(height: 400)
-                    .background(.yellow)
-                Text("CHILDREN")
-                    .frame(height: 400)
-                    .background(.yellow)
-                Text("CHILDREN")
-                    .frame(height: 400)
-                    .background(.yellow)
-                Text("CHILDREN")
-                    .frame(height: 400)
-                    .background(.yellow)
-                Text("CHILDREN")
-                    .frame(height: 400)
-                    .background(.yellow)
+                ANReminderCard(icon: "doc", title: "Meal Time", time: "10.00", frequency: "Every Day")
+                ANReminderCard(icon: "doc", title: "Meal Time", time: "10.00", frequency: "Every Day")
+                ANReminderCard(icon: "doc", title: "Meal Time", time: "10.00", frequency: "Every Day")
+                ANReminderCard(icon: "doc", title: "Meal Time", time: "10.00", frequency: "Every Day")
             }
-            .padding(.horizontal)
+            .padding()
         })
+        .sheet(isPresented: $isSheetPresented) {
+            VStack {
+                ANToolbar(leading: {
+                    Text("Cancel")
+                        .font(.toolbar)
+                        .foregroundColor(Color.anNavigation)
+                        .onTapGesture {
+                            closeReminderForm()
+                        }
+                }, title: "Add Reminder") {
+                    Text("Save")
+                        .font(.toolbar)
+                        .foregroundColor(Color.anNavigation)
+                        .onTapGesture {
+                            closeReminderForm()
+                        }
+                }
+                
+                VStack {
+                    ANActivitySelector()
+                    ANTextField(text: .constant(""), placeholder: "Add reminder title...", label: "Title")
+                    
+                    ANTextFieldArea(text: .constant(""), label: "Description", placeholder: "Add description...")
+                        .frame(height: 200)
+                    
+                    ANTimePicker(time: .constant(Date()), label: "Time")
+                    
+                    ANDatePicker(date: .constant(Date()), label: "Repeat")
+                }
+                .padding()
+                
+                Spacer()
+            }
+            .padding(.vertical)
+        }
     }
 }
 
