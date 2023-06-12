b312afff748a84f3eb448675717dc8b4664c1bb2
Roli Bernanda <acappemborong@gmail.com>
2023-06-12 10:55:09 +0700

    Enhance: Grouping upcoming events

--- Antimo/Extensions/FetchResults+Extension.swift
+++ Antimo/Extensions/FetchResults+Extension.swift
@@ -33,3 +33,31 @@ extension FetchedResults<Activity> {
         return OrderedActivity(activities: dictOfActivities, keys: keyOfDict)
     }
 }
+
+
+struct OrderedEvent {
+    var events = [String:[Event]]()
+    var keys = [String]()
+}
+
+extension FetchedResults<Event> {
+    var byDate:OrderedEvent {
+        var dictOfEvents = [String:[Event]]()
+        var keyOfDict = [String]()
+        
+        for event in self {
+            let key = Utilities.formattedDate(from: event.reminder?.createdAt ?? Date(), format: "EEEE, d MMM yyyy")
+            
+            if var dict = dictOfEvents[key] {
+                dict.append(event)
+                
+                dictOfEvents[key] = dict
+            } else {
+                keyOfDict.append(key)
+                dictOfEvents[key] = [event]
+            }
+        }
+        
+        return OrderedEvent(events: dictOfEvents, keys: keyOfDict)
+    }
+}
