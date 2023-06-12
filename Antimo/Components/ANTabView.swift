dd4d2de7b157c8564f691ab882f6204a7de91faf
Roli Bernanda <acappemborong@gmail.com>
2023-06-13 00:27:12 +0700

    Enhance: ability to popping to the root on double tap in the tabitem

--- Antimo/Components/ANTabView.swift
+++ Antimo/Components/ANTabView.swift
@@ -22,90 +22,105 @@ struct ANTabView: View {
     @ObservedObject var activityNavigation: ActivityNavigationManager
     
     var body: some View {
-            TabView(selection: $selectedTab) {
-                NavigationStack(path: $dashboardNavigation.dashboardPaths) {
-                    SummaryView()
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .navigationDestination(for: DashboardRoute.self) { route in
-                            switch(route) {
-                            case .allEvents:
-                                DashboardAllEventView()
-                            }
-                        }
-                }
-                
-                .tabItem {
-                    Label {
-                        Text("Dashboard")
-                            .font(.tab)
-                    } icon: {
-                        Image(systemName: "rectangle.3.group.fill")
-                    }
-                }
-                .tag(NavigationTabs.dashboard)
-                .environmentObject(dashboardNavigation)
-                
-                NavigationStack(path: $journalNavigation.journalPaths) {
-                    JournalView()
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .navigationDestination(for: JournalRoute.self) { route in
-                            switch(route) {
-                            case .addJournal:
-                                AddJournalView()
-                            }
-                        }
-                }
-                .tabItem {
-                    Label {
-                        Text("Journal")
-                            .font(.tab)
-                    } icon: {
-                        Image(systemName: "text.book.closed.fill")
-                    }
-                }
-                .tag(NavigationTabs.journal)
-                .environmentObject(journalNavigation)
-                    
-                
-                NavigationStack(path: $activityNavigation.activityPaths) {
-                    ActivityView()
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .navigationDestination(for: ActivityRoute.self) { route in
-                            switch(route) {
-                            case .allEvents:
-                                AllEventView()
-                            case let .activitesPerDate(selectedDate):
-                                ActivityDetailsView(selectedDate: selectedDate)
-                            case .addJournal:
-                                ActivityAddJournalView()
-                            }
-                        }
-                }
-                .tabItem {
-                    Label {
-                        Text("Event")
-                            .font(.tab)
-                    } icon: {
-                        Image(systemName: "calendar")
-                    }
-                }
-                .tag(NavigationTabs.event)
-                .environmentObject(activityNavigation)
-                
-                ReminderView()
+        TabView(selection: $selectedTab) {
+            NavigationStack(path: $dashboardNavigation.dashboardPaths) {
+                SummaryView()
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .tabItem {
-                        Label {
-                            Text("Reminder")
-                                .font(.tab)
-                        } icon: {
-                            Image(systemName: "alarm.fill")
+                    .navigationDestination(for: DashboardRoute.self) { route in
+                        switch(route) {
+                        case .allEvents:
+                            DashboardAllEventView()
                         }
                     }
-                    .tag(NavigationTabs.reminder)
-                
             }
-            .tint(Color.anPrimary)
-            .preferredColorScheme(.light)
+            
+            .tabItem {
+                Label {
+                    Text("Dashboard")
+                        .font(.tab)
+                } icon: {
+                    Image(systemName: "rectangle.3.group.fill")
+                }
+            }
+            .tag(NavigationTabs.dashboard)
+            .environmentObject(dashboardNavigation)
+            
+            NavigationStack(path: $journalNavigation.journalPaths) {
+                JournalView()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .navigationDestination(for: JournalRoute.self) { route in
+                        switch(route) {
+                        case .addJournal:
+                            AddJournalView()
+                        }
+                    }
+            }
+            .tabItem {
+                Label {
+                    Text("Journal")
+                        .font(.tab)
+                } icon: {
+                    Image(systemName: "text.book.closed.fill")
+                }
+            }
+            .tag(NavigationTabs.journal)
+            .environmentObject(journalNavigation)
+            
+            
+            NavigationStack(path: $activityNavigation.activityPaths) {
+                ActivityView()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .navigationDestination(for: ActivityRoute.self) { route in
+                        switch(route) {
+                        case .allEvents:
+                            AllEventView()
+                        case let .activitesPerDate(selectedDate):
+                            ActivityDetailsView(selectedDate: selectedDate)
+                        case .addJournal:
+                            ActivityAddJournalView()
+                        }
+                    }
+            }
+            .tabItem {
+                Label {
+                    Text("Event")
+                        .font(.tab)
+                } icon: {
+                    Image(systemName: "calendar")
+                }
+            }
+            .tag(NavigationTabs.event)
+            .environmentObject(activityNavigation)
+            
+            ReminderView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .tabItem {
+                    Label {
+                        Text("Reminder")
+                            .font(.tab)
+                    } icon: {
+                        Image(systemName: "alarm.fill")
+                    }
+                }
+                .tag(NavigationTabs.reminder)
+            
+        }
+        .tint(Color.anPrimary)
+        .preferredColorScheme(.light)
+        .gesture(TapGesture(count: 2).onEnded {
+            switch selectedTab {
+            case .dashboard:
+                dashboardNavigation.reset()
+                break
+            case .journal:
+                journalNavigation.reset()
+                break
+            case .event:
+                activityNavigation.reset()
+                break
+            case .reminder:
+                break
+            }
+        })
     }
 }
