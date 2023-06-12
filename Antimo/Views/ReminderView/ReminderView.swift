c13b0f84983f3f13e931c7f02dee274a4ba37e66
Roli Bernanda <acappemborong@gmail.com>
2023-06-12 16:14:49 +0700

    ANT-210: handle no routine yet

--- Antimo/Views/ReminderView/ReminderView.swift
+++ Antimo/Views/ReminderView/ReminderView.swift
@@ -24,27 +24,44 @@ struct ReminderView: View {
                     .onTapGesture { vm.openReminderForm() }
             }
         }, children: {
-            
-            ScrollView {
-                ForEach(routines) { routine in
-                    ANReminderCard(
-                        icon: vm.getIcon(routine.reminder?.type ?? ""),
-                        title: routine.reminder?.title ?? "",
-                        time: vm.getRenderedHourAndMinutes(routine.getWeekdays.first?.time ?? Date()),
-                        frequency: vm.getRenderedFrequency(vm.convertWeekDaysObjIntoInt(routine.getWeekdays)),
-                        isOn: routine.reminder?.isActive ?? false,
-                        onToggle: { newValue in
-                            vm.toggleActivation(
-                                reminder: routine.reminder!,
-                                viewContext: viewContext,
-                                notificationManager: notificationManager,
-                                newValue: newValue
-                            )
-                        }
-                    )
+            if routines.isEmpty {
+                Spacer()
+
+                VStack(spacing: 10) {
+                    Text("You don't have any reminders yet, Let's add some now")
+                        .font(.placeholder)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                    
+                    Text("Add Routine")
+                        .font(.toolbar)
+                        .foregroundColor(Color.anNavigation)
+                        .onTapGesture { vm.openReminderForm() }
                 }
+
+                Spacer()
+            } else {
+                ScrollView {
+                    ForEach(routines) { routine in
+                        ANReminderCard(
+                            icon: vm.getIcon(routine.reminder?.type ?? ""),
+                            title: routine.reminder?.title ?? "",
+                            time: vm.getRenderedHourAndMinutes(routine.getWeekdays.first?.time ?? Date()),
+                            frequency: vm.getRenderedFrequency(vm.convertWeekDaysObjIntoInt(routine.getWeekdays)),
+                            isOn: routine.reminder?.isActive ?? false,
+                            onToggle: { newValue in
+                                vm.toggleActivation(
+                                    reminder: routine.reminder!,
+                                    viewContext: viewContext,
+                                    notificationManager: notificationManager,
+                                    newValue: newValue
+                                )
+                            }
+                        )
+                    }
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
         })
         .onAppear { notificationManager.request() }
         .sheet(isPresented: $vm.isReminderFormPresented) {
